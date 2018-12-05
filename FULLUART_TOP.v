e5cf9afc74279ae4a41d52e8517e96e8ba5a0760
stevensfans <1anh21@gmail.com>
2018-12-05 10:26:32 -0800

    progress for top level

---
+++ FULLUART_TOP.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:15:02 11/22/2018 
+// Design Name: 
+// Module Name:    FULLUART 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FULLUART(CLK, RESET, EIGHT, PEN, OHEL, BAUD, READ_STROBE, WRITE_STROBE,
+					 RX, TX, IN_PORT,OUT_PORT);
+
+
+endmodule
